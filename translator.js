c068ffc6e7cc1d3f4a3252593bd7022bb70bba1d
SuperPhantomUser <74560404+SuperPhantomUser@users.noreply.github.com>
2021-05-14 13:18:59 -0400

    Update translator.js

--- translator.js
+++ translator.js
@@ -1,1 +1,46 @@
-const 
+const vowels = ["a", "e", "i", "o", "u", "y"];
+const notVowels = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
+
+function removeEndPunctuation(word) {
+  let lasts = [];
+  while (!vowels.includes(word[word.length - 1]) && !notVowels.includes(word[word.length - 1])) {
+    let last = word[word.length - 1];
+    lasts.push(last);
+    word = word.slice(0, -1);
+  }
+  return [lasts, word];
+}
+
+function translateWord(word) {
+  let extra = "";
+  if (word.includes("'")) {
+    let splitted = word.split("'");
+    extra = splitted[1];
+    word = splitted[0];
+  }
+  let multipleReturns = removeEndPunctuation(word);
+  let lasts = multipleReturns[0];
+  word = multipleReturns[1];
+  let last = word[word.length - 1];
+  word = word.slice(0, -1);
+  if (vowels.includes(last)) word = word + "egg";
+  else word = word + "egge";
+  word = word + last;
+  if (extra != "") word = word + "'" + extra;
+  if (lasts.length > 0) {
+    for (let item of lasts) {
+      word = word + item;
+    }
+  }
+  return word;
+}
+
+function englishToEgglish(content) {
+  let words = content.split(" ");
+  let translatedArray = [];
+  for (let word of words) {
+    translatedArray.push(translateWord(word));
+  }
+  let translated = translatedArray.join(" ");
+  return translated;
+}
